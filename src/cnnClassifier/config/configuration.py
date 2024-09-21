68b8134ab18811d6152f5606811a671ff3f338ed
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2024-09-21 13:14:50 +0500

    prepare callbacks stage completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
-from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
+from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig, PrepareCallbacksConfig)
 import os
 
 class ConfigurationManager:
@@ -18,7 +18,7 @@ class ConfigurationManager:
         # create_directories([self.config.artifacts_root])
 
 
-    
+    # Stage 1
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -35,7 +35,7 @@ class ConfigurationManager:
 
         return data_ingestion_config
 
-    
+    # Stage 2
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
 
@@ -56,3 +56,20 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+    # Stage 3
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+            config = self.config.prepare_callbacks
+            model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+            create_directories([
+                Path(model_ckpt_dir),
+                Path(config.tensorboard_root_log_dir)
+            ])
+
+            prepare_callback_config = PrepareCallbacksConfig(
+                root_dir=Path(config.root_dir),
+                tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+                checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+            )
+
+            return prepare_callback_config
